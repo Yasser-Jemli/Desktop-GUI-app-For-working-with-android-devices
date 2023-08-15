d1c84a2d16302c501a408e6160f766e0c30660f7
Yasser-Jemli <yasserjamli37@gmail.com>
2023-08-15 22:10:20 +0100

    from-15-08-22:10

--- Myapplication.py
+++ Myapplication.py
@@ -1,19 +1,59 @@
 from tkinter import *
-from tkinter.font import Font 
+
+window =Tk()
+
+# ******* Create Terminal Output Area ********************************
+def create_output_area(self):
+        self.output_text = tk.Text(self.root, wrap=tk.WORD, font=('Courier', 12))
+        self.output_text.pack(padx=10, pady=5, fill=tk.BOTH, expand=True)
+#---------------------------------------------------------------------------------
+
+def execute_command( command):
+        try:
+            result = subprocess.run(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            output = result.stdout + result.stderr
+        except Exception as e:
+            output = f"Error: {str(e)}"
+
+def append_output( text):
+        output_text.insert(tk.END, text)
+    
+
+def clear_output():
+        output_text.delete(1.0, tk.END)
+
+def list_devices():
+        output = execute_command("adb devices")
+        append_output(output)
 
 
-window=Tk()
-window.title("my android monitor")
-window.geometry("960x960")
-#color declaration 
+# B1 :  to show a adb devices >> list & state 
+#************************************ adb devices *********************************************
+b1=Button(window,text='adb_devices',command=list_devices)
+b1.grid(row=0,column=10,rowspan=1,columnspan=1,sticky='nsew')
 
-# Hi i am Yasser 
-# labels declaration 
-main_label = Label(window,text='welcome to our application',bg='green').pack()
 
-# button declaration 
+#***********************************************************************************************
+# B2 : Configuration CAN-0
 
-adb_devices=Button(window, text='adb devices',width=10,pady= 5 ,padx =5,font=Font(family='tahome', size =10, weight='bold')).pack()
+b2=Button(window,text='CAN_Configuration : CAN-0')
+b2.grid(row=0,column=0,rowspan=1,columnspan=1,sticky='nsew')
+
+# B3 : Configuration CAN-1
+
+b3=Button(window,text='CAN_Configuration : CAN-1')
+b3.grid(row=1,column=0,rowspan=1,columnspan=1,sticky='nsew')
+
+# Create Volume + : Works via adb commands 
+
+b4=Button(window, text='volume plus 1 +')
+b4.grid(row=6,column=0,rowspan=1,columnspan=1,sticky='nsew')
+
+# Create Volume - : Works via adb commands
+
+b5=Button(window,text='volume minus 1 -') 
+b5.grid(row=7,column=0,rowspan=1,columnspan=1,sticky='nsew')
+
 
 
 window.mainloop()
---
+++ button_template.py
@@ -0,0 +1,15 @@
+from tkinter import * 
+
+
+window =Tk()
+
+# Create a button
+
+b1=Button(text='Hello')
+b1.pack()
+b1.grid(row=0,column=0)
+
+
+# ... add more widgets and configurations to the adb_devices Frame
+
+window.mainloop()
