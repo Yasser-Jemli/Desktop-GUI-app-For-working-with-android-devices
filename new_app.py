ba8c9acd7ab24b81616662fb35dc3240e0008f85
Yasser-Jemli <yasserjamli37@gmail.com>
2023-08-17 22:14:17 +0200

    new way

--- new_app.py
+++ new_app.py
@@ -1,7 +1,12 @@
-from tkinter import *
+import customtkinter # you should install pip3 install customtkinter
 import subprocess
 import threading
 
+customtkinter.set_appearance_mode("light")  # Modes: system (default), light, dark
+customtkinter.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
+
+root = customtkinter.CTk(fg_color="grey")  # create CTk window like you do with the Tk window
+root.geometry("1024x1024")
 class ScrcpyController:
     def __init__(self, root):
         self.root = root
@@ -11,32 +16,33 @@ class ScrcpyController:
         
         self.create_buttons()
         self.create_output_terminal()
-        
+
+    
     def create_buttons(self):
-        self.b8 = Button(self.root, text="Start Scrcpy", command=self.start_scrcpy)
+        self.b8 = customtkinter.CTkButton(master=self.root, text="Start Scrcpy", command=self.start_scrcpy,border_color="dark")
         self.b8.grid(row=0, column=0, rowspan=1, columnspan=1)
         
-        self.b9 = Button(self.root, text="Stop Scrcpy", command=self.stop_scrcpy, state=DISABLED)
+        self.b9 = customtkinter.CTkButton(master=self.root, text="Stop Scrcpy",fg_color='red', command=self.stop_scrcpy, state="disabled", border_color="dark")
         self.b9.grid(row=1, column=0, rowspan=1, columnspan=1)
         
-        self.b7 = Button(self.root, text='Clear Terminal Output', command=self.clear_output)
+        self.b7 = customtkinter.CTkButton(master=self.root, text='Clear Terminal Output', command=self.clear_output,border_color="dark")
         self.b7.grid(row=0, column=1, rowspan=1, columnspan=1)
         
-        self.b1 = Button(self.root, text='adb_devices')
+        self.b1 = customtkinter.CTkButton(master=self.root, text='adb_devices',border_color="dark")
         self.b1.grid(row=1, column=1, rowspan=1, columnspan=1)
         
-        self.b6 = Button(self.root, text='profiles')
+        self.b6 = customtkinter.CTkButton(self.root, text='profiles',border_color="dark")
         self.b6.grid(row=2, column=1, rowspan=1, columnspan=1)
         
     def create_output_terminal(self):
-        self.output_text = Text(self.root, wrap=WORD, font=('Courier', 12))
+        self.output_text = customtkinter.CTkTextbox(master=self.root,font=('Courier', 12),border_color="dark")
         self.output_text.grid(row=0, column=2, rowspan=2, columnspan=2, sticky='nsew')
         
     def append_output(self, text):
-        self.output_text.insert(END, text)
+        self.output_text.insert("end", text)
         
     def clear_output(self):
-        self.output_text.delete(1.0, END)
+        self.output_text.delete(1.0, "end")
         
     def run_scrcpy(self):
         self.scrcpy_process = subprocess.Popen(["scrcpy"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
@@ -49,12 +55,12 @@ class ScrcpyController:
             
         else:
             self.append_output('we encountered issues while trying to launch scrcpy\n')
-        self.b8.config(state=NORMAL)
-        self.b9.config(state=DISABLED)
+        self.b8.configure(state="normal")
+        self.b9.configure(state="disabled")
 
     def start_scrcpy(self):
-        self.b8.config(state=DISABLED)
-        self.b9.config(state=NORMAL)
+        self.b8.configure(state="disabled")
+        self.b9.configure(state="normal")
 
         scrcpy_thread = threading.Thread(target=self.run_scrcpy)
         scrcpy_thread.start()
@@ -65,10 +71,11 @@ class ScrcpyController:
             self.scrcpy_process.terminate()
             self.scrcpy_process.wait()  # Wait for the process to finish
 
-        self.b8.config(state=NORMAL)
-        self.b9.config(state=DISABLED)
+        self.b8.configure(state="normal")
+        self.b9.configure(state="disabled")
+
 
 if __name__ == "__main__":
-    root = Tk()
+    
     controller = ScrcpyController(root)
     root.mainloop()
