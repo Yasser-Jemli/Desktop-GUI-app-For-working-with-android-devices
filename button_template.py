2142e9425479d6c339a7eb11123e6e2548c5e9d2
Yasser-Jemli <yasserjamli37@gmail.com>
2023-08-17 19:40:34 +0200

    the other way

--- button_template.py
+++ button_template.py
@@ -9,7 +9,8 @@ b1=Button(text='Hello')
 b1.pack()
 b1.grid(row=0,column=0)
 
-
+# Note Grid and Pack and Place are three window Managers shouldn't be used in the same application 
+# that's why in our projec we will use only grid   
 # ... add more widgets and configurations to the adb_devices Frame
 
 window.mainloop()
