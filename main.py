bad39b63068025f0ca03f4361cd25cd9e2487dee
yasser.jamli <yasser.jamli@celadodc-rswl.com>
2023-08-27 19:45:57 +0200

    Start working

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk 
 from tkinter  import ttk
+import customtkinter
 import subprocess
 import threading
 
@@ -19,8 +20,31 @@ style.theme_use("forest-dark")
 frame = ttk.Frame(root)
 frame.pack()
 
+# scrcpy functions 
 
-# our button functions 
+def run_scrcpy():
+        scrcpy_process = subprocess.Popen(["scrcpy"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+        scrcpy_process.wait()  # Wait for the process to finish
+
+        return_code = scrcpy_process.returncode
+        if return_code == 0:
+            append_output('Closing scrcpy ........\n')
+
+        else:
+            append_output('we encountered issues while trying to launch scrcpy\n')
+        b3.configure(state="normal")
+        b4.configure(state="disabled")
+
+def start_scrcpy():
+        b3.configure(state="disabled")
+        b4.configure(state="normal")
+
+        scrcpy_thread = threading.Thread(target=run_scrcpy)
+        scrcpy_thread.start()
+        append_output('Starting Scrcpy 99%.... \n')
+
+# our button functions -- adb commands 
 
 
 def list_profiles():
@@ -103,13 +127,14 @@ button_frame_label = ttk.LabelFrame(frame,text="My ADB Commands")
 button_frame_label.grid(row=0, column=0, padx=20 , pady=10)
 
 terminal_frame = ttk.LabelFrame(frame, text="My terminal")
-terminal_frame.grid(row=0,column=1,)
+terminal_frame.grid(row=0,column=1, padx=20 , pady=20)
 
 
 # My terminal function 
 
-output_text = ttk.Treeview(terminal_frame)
-output_text.pack()
+
+output_text = customtkinter.CTkTextbox(terminal_frame, width=600 ,height=200)
+output_text.grid(row=0, column=0 , sticky="nsew")
 
 # output_text.grid(row=1 , column=0 , sticky="nsew")
         
@@ -121,19 +146,19 @@ def clear_output():
 
 # button for the button_frame_label are definied Here 
 
-b1 = ttk.Button(button_frame_label, text=" list _ adb _devices + states ")
+b1 = ttk.Button(button_frame_label, text=" list _ adb _devices + states ", command=list_devices)
 b1.grid(row=1 , column=0 ,padx=5, pady=5, sticky="nsew")
-b2 = ttk.Button(button_frame_label , text=" list _ profiles ")
+b2 = ttk.Button(button_frame_label , text=" list _ profiles ", command=list_profiles)
 b2.grid(row=2 , column=0 ,padx=5, pady=5, sticky="nsew")
-b3 = ttk.Button(button_frame_label, text=" Start Scrcpy ")
+b3 = ttk.Button(button_frame_label, text=" Start Scrcpy ",command=start_scrcpy)
 b3.grid(row=3 , column=0 ,padx=5, pady=5, sticky="nsew")
 b4 = ttk.Button(button_frame_label , text=" Stop Scrcpy ")
 b4.grid(row=4 , column=0 ,padx=5, pady=5, sticky="nsew")
-b5 = ttk.Button(button_frame_label, text=" Volume Up ")
+b5 = ttk.Button(button_frame_label, text=" Volume Up ",command=run_volume_plus)
 b5.grid(row=5 , column=0 ,padx=5, pady=5, sticky="nsew")
-b6 = ttk.Button(button_frame_label , text=" Volume Down ")
+b6 = ttk.Button(button_frame_label , text=" Volume Down ",command=run_volume_minus)
 b6.grid(row=6 , column=0 ,padx=5, pady=5, sticky="nsew")
-b7 = ttk.Button(button_frame_label, text="Clear Terminal")
+b7 = ttk.Button(button_frame_label, text="Clear Terminal",command=clear_output)
 b7.grid(row=7 , column=0 ,padx=5, pady=5, sticky="nsew")
 
 # Window Loop 
